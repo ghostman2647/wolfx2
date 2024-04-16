b5641685426c3099ea0fe7edafdde3e6ce931310
ghostman2647 <166911473+ghostman2647@users.noreply.github.com>
2024-04-16 08:58:51 -0300

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -230,7 +230,7 @@ export class Client {
       this.apiBaseHeaders = {
          origin: "https://game.bombcrypto.io",
          referer: "https://game.bombcrypto.io",
-         "sec-ch-ua": ` " Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"`,
+         "sec-ch-ua": ` " Not A;Brand";v="8", "Chromium";v="123", "Google Chrome";v="123"`,
          "sec-ch-ua-mobile": "?0",
          "sec-ch-ua-platform": `"Windows"`,
          "sec-fetch-dest": "empty",
